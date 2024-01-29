4edba0068cea1db82449b4b103ac7445b2faabcf
KN-sunil <sunilsun837@gmail.com>
2024-01-29 22:17:56 +0530

    stringliteral

---
+++ iterativefor/stringliteral.js
@@ -0,0 +1,9 @@
+//string literal
+  // reading a avriable in the form of string
+
+  let eid=102;
+  let ename="rahul"
+  let esal=25000
+
+  //console.log("employee id:",eid,"name=",ename)
+  console.log(`employee id ${eid} name ${ename}`)
