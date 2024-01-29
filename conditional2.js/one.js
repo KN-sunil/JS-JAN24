2ed9d7df031f85cd845fb7c27518d4731ad0c87b
KN-sunil <sunilsun837@gmail.com>
2024-01-29 21:03:59 +0530

    2

---
+++ conditional2.js/one.js
@@ -0,0 +1,9 @@
+//program to print given number is 3 digit number or not
+
+let num=765;
+if(num>100 && num<999){
+    console.log("3 digit number")
+}
+else {
+    console.log("not a 3 digit number")
+}
---
+++ conditional2.js/two.js
@@ -0,0 +1,6 @@
+let num=34;
+
+//condition st? true statement : false statement
+
+num>=100 && num<=999?console.log("3 digit number"):
+                    console.log("not a 3 digit number")
