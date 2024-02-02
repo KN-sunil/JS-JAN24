b41be848354b9228d68877eaf88b5d1292e600e1
KN-sunil <sunilsun837@gmail.com>
2024-02-02 13:14:31 +0530

    fn

---
+++ functions/four.js
@@ -0,0 +1,22 @@
+//Creating function with arguments and return value
+
+/* function add(a,b,c){
+
+      return a+b+c;
+   
+} let result=add(20,30,40)
+ console.log(result) */
+
+//---------------------------------------------------------------------------------------------
+
+/* function add(a,b,c,d){
+     c=10;
+     d=20;
+
+    return a+b+c+d
+}
+let result=add(30,40)
+console.log(result) */
+
+
+
---
+++ functions/three.js
@@ -0,0 +1,8 @@
+//creating function with agruments and default values also
+function add(a,b,c=1){
+
+console.log(a+b+c)
+}
+add(20,30,60)
+add(20,30,60)
+add(20,4)
