a6092b53418791a1e2ef34bbeae49ebc90b074a5
KN-sunil <sunilsun837@gmail.com>
2024-02-05 20:27:02 +0530

    .

---
+++ array methods/three.js
@@ -0,0 +1,9 @@
+//unshift()-appends or insert new element at the begining of the array 
+//and returns it
+
+let products=["iphone","relame","redmi","vivo"]
+
+//if we want add "samsung" at the beginning of the array 
+products.unshift("samsung")
+
+console.log(products)
