fc2f91fc3356db13c602f10a99a4a5496ea8968d
KN-sunil <sunilsun837@gmail.com>
2024-02-05 20:37:05 +0530

    ..

---
+++ array methods/four.js
@@ -0,0 +1,6 @@
+//shift()-deletes the first element of the array and returns array
+
+let names=["prabhas","anushka","samantha","ileana","rashmika"]
+
+names.shift()
+console.log(names)
---
+++ array methods/six.js
@@ -0,0 +1,5 @@
+let enames=["suni","ganesh","shashank","abhi",]
+enames.reverse()
+
+console.log(enames)
+//we can reverse the array using reverse() method
