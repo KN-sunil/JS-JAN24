7ac1a30333fa6d3229284d60bdb9f67972517083
KN-sunil <sunilsun837@gmail.com>
2024-02-05 20:21:26 +0530

    .

--- array methods/one.js
+++ array methods/one.js
@@ -1,9 +1,16 @@
 //push-appends/add new element at the end of array and returns
-
+/* 
 var size=[]
 
 size.push(20);
 size.push(30);
 size.push(40);
 size.push(50);
-console.log(size)
+console.log(size) */
+
+let ids=[101,102,103,104]
+
+
+ids.push(ids[5])
+ids.push("sunil")//if we want push elements at particular index 
+console.log(ids)
---
+++ array methods/two.js
@@ -0,0 +1,11 @@
+//pop()-remove element last value of the array and returns
+
+/* let names=["rahul","sonia","priyanka","habibi"]
+names.pop("habibi")
+console.log(names) */
+
+
+let ids=[101,102,103,104,105]
+
+ids.pop()
+console.log(ids)
