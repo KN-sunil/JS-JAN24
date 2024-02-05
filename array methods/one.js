4c2a5064920109260f58e6edd1e8892d45c93930
KN-sunil <sunilsun837@gmail.com>
2024-02-05 20:11:05 +0530

    okie

---
+++ array methods/one.js
@@ -0,0 +1,9 @@
+//push-appends/add new element at the end of array and returns
+
+var size=[]
+
+size.push(20);
+size.push(30);
+size.push(40);
+size.push(50);
+console.log(size)
---
+++ functions/web/data.js
@@ -0,0 +1,6 @@
+function wish(){
+let a=10
+let b=20
+let c=30
+console.log("good morning")
+}
