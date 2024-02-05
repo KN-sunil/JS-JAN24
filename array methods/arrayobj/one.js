b2018a6553145d2fd7d22e66ecce4d7aa158c759
KN-sunil <sunilsun837@gmail.com>
2024-02-05 20:48:42 +0530

    ...

---
+++ array methods/arrayobj/one.js
@@ -0,0 +1,13 @@
+//we cant use length property in objects
+//if we want to check given object is empty or not we should use object.keys()
+//method
+//if we use object.keys() internally it converts objects into array and returns it
+
+let box={id:101,}
+
+if(Object.keys(box).length >0){
+    console.log("Not Empty box")
+}
+else{
+    console.log("Empty box")
+}
