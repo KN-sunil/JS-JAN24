74611605bdf68ff51bd4894e35c2233f904a8989
KN-sunil <sunilsun837@gmail.com>
2024-02-07 18:29:41 +0530

    assign

---
+++ employees data/data.js
@@ -0,0 +1,132 @@
+let employees=
+    [{"id":1,"name":"Lodovico","email":"lcerro0@state.gov","gender":"Male"},
+{"id":2,"name":"Marmaduke","email":"mlangdale1@nymag.com","gender":"Male"},
+{"id":3,"name":"Lorene","email":"lblazek2@google.com","gender":"Female"},
+{"id":4,"name":"Tamas","email":"tskelly3@list-manage.com","gender":"Male"},
+{"id":5,"name":"Bradley","email":"button4@technorati.com","gender":"Male"},
+{"id":6,"name":"Ezra","email":"ecausey5@zimbio.com","gender":"Male"},
+{"id":7,"name":"Cody","email":"cbuttrum6@dell.com","gender":"Male"},
+{"id":8,"name":"Claudio","email":"ctadgell7@acquirethisname.com","gender":"Polygender"},
+{"id":9,"name":"Catarina","email":"cfinlason8@ted.com","gender":"Female"},
+{"id":10,"name":"Cathrine","email":"cboyan9@comsenz.com","gender":"Female"},
+{"id":11,"name":"Addie","email":"agusha@vistaprint.com","gender":"Female"},
+{"id":12,"name":"Loralie","email":"lcoulstonb@purevolume.com","gender":"Female"},
+{"id":13,"name":"Finlay","email":"fnobletc@jalbum.net","gender":"Male"},
+{"id":14,"name":"Walliw","email":"wkanterd@nydailynews.com","gender":"Female"},
+{"id":15,"name":"Beatriz","email":"bcusworthe@bigcartel.com","gender":"Female"},
+{"id":16,"name":"Clemmy","email":"ckirtlanf@istockphoto.com","gender":"Male"},
+{"id":17,"name":"Kitti","email":"kwilflingg@loc.gov","gender":"Female"},
+{"id":18,"name":"Beatrisa","email":"bketcherh@chicagotribune.com","gender":"Female"},
+{"id":19,"name":"Zerk","email":"zalvisi@altervista.org","gender":"Male"},
+{"id":20,"name":"Rowen","email":"rchippsj@printfriendly.com","gender":"Male"},
+{"id":21,"name":"Chad","email":"cjuschkak@google.com.hk","gender":"Female"},
+{"id":22,"name":"Benjie","email":"blotonl@discovery.com","gender":"Male"},
+{"id":23,"name":"Shelli","email":"sfratsonm@vinaora.com","gender":"Female"},
+{"id":24,"name":"Godard","email":"giwanczykn@meetup.com","gender":"Male"},
+{"id":25,"name":"Valentine","email":"vpaolilloo@ovh.net","gender":"Male"},
+{"id":26,"name":"Colver","email":"crootep@w3.org","gender":"Male"},
+{"id":27,"name":"Chance","email":"cbrafieldq@bbc.co.uk","gender":"Male"},
+{"id":28,"name":"Peirce","email":"podonohuer@usa.gov","gender":"Male"},
+{"id":29,"name":"Querida","email":"qmatyukons@weebly.com","gender":"Female"},
+{"id":30,"name":"Benni","email":"bgildersleevet@storify.com","gender":"Female"},
+{"id":31,"name":"Nil","email":"nfettersu@soundcloud.com","gender":"Male"},
+{"id":32,"name":"Shalom","email":"scadamyv@sun.com","gender":"Male"},
+{"id":33,"name":"Golda","email":"gpetherw@ycombinator.com","gender":"Female"},
+{"id":34,"name":"Creighton","email":"cconstantx@goo.gl","gender":"Male"},
+{"id":35,"name":"Layton","email":"lmacswaydey@prweb.com","gender":"Male"},
+{"id":36,"name":"Lemar","email":"lsantoriz@dropbox.com","gender":"Male"},
+{"id":37,"name":"Gustie","email":"gayrton10@4shared.com","gender":"Female"},
+{"id":38,"name":"Gregory","email":"gcherrie11@wikipedia.org","gender":"Genderfluid"},
+{"id":39,"name":"Hansiain","email":"hhillam12@simplemachines.org","gender":"Male"},
+{"id":40,"name":"Ceil","email":"cfowler13@opera.com","gender":"Female"},
+{"id":41,"name":"Brigham","email":"bbramhill14@tuttocitta.it","gender":"Male"},
+{"id":42,"name":"Dyanna","email":"deyam15@deliciousdays.com","gender":"Female"},
+{"id":43,"name":"Laurella","email":"lpoulton16@pagesperso-orange.fr","gender":"Polygender"},
+{"id":44,"name":"Jeanne","email":"jradnage17@hc360.com","gender":"Female"},
+{"id":45,"name":"Freedman","email":"fhaddleston18@tumblr.com","gender":"Male"},
+{"id":46,"name":"Brandtr","email":"bmarjanovic19@loc.gov","gender":"Male"},
+{"id":47,"name":"Starr","email":"ssmallman1a@hp.com","gender":"Polygender"},
+{"id":48,"name":"Joellyn","email":"jdorman1b@squarespace.com","gender":"Female"},
+{"id":49,"name":"Kris","email":"kkollaschek1c@ebay.co.uk","gender":"Male"},
+{"id":50,"name":"Tarrah","email":"tdoy1d@aboutads.info","gender":"Female"},
+{"id":51,"name":"Israel","email":"iharmon1e@technorati.com","gender":"Genderfluid"},
+{"id":52,"name":"Janith","email":"jcoldtart1f@rambler.ru","gender":"Female"},
+{"id":53,"name":"Timmy","email":"tdanielis1g@elpais.com","gender":"Male"},
+{"id":54,"name":"Torr","email":"tgilsthorpe1h@time.com","gender":"Male"},
+{"id":55,"name":"Nathanil","email":"ncrosland1i@upenn.edu","gender":"Male"},
+{"id":56,"name":"Laurianne","email":"lcremin1j@ox.ac.uk","gender":"Female"},
+{"id":57,"name":"Sean","email":"scharrette1k@archive.org","gender":"Male"},
+{"id":58,"name":"Myra","email":"mcrayton1l@ebay.co.uk","gender":"Female"},
+{"id":59,"name":"Cleve","email":"celnaugh1m@boston.com","gender":"Male"},
+{"id":60,"name":"Denice","email":"dquilkin1n@google.ru","gender":"Female"},
+{"id":61,"name":"Elicia","email":"echriston1o@harvard.edu","gender":"Female"},
+{"id":62,"name":"Darcy","email":"dizkoveski1p@slate.com","gender":"Male"},
+{"id":63,"name":"Antony","email":"aedinborough1q@google.co.jp","gender":"Male"},
+{"id":64,"name":"Editha","email":"ejarrelt1r@china.com.cn","gender":"Polygender"},
+{"id":65,"name":"Theda","email":"ttavener1s@washingtonpost.com","gender":"Female"},
+{"id":66,"name":"Anatole","email":"aroglieri1t@nytimes.com","gender":"Male"},
+{"id":67,"name":"Shandee","email":"sstickel1u@exblog.jp","gender":"Female"},
+{"id":68,"name":"Harman","email":"harundell1v@cam.ac.uk","gender":"Male"},
+{"id":69,"name":"Paul","email":"pleon1w@geocities.com","gender":"Male"},
+{"id":70,"name":"Piper","email":"ptruce1x@nydailynews.com","gender":"Female"},
+{"id":71,"name":"Kylie","email":"kjeskins1y@topsy.com","gender":"Male"},
+{"id":72,"name":"Peggie","email":"pmccartan1z@a8.net","gender":"Female"},
+{"id":73,"name":"Raffaello","email":"rlackham20@archive.org","gender":"Male"},
+{"id":74,"name":"Christye","email":"clytlle21@bluehost.com","gender":"Female"},
+{"id":75,"name":"Ronny","email":"rstuer22@g.co","gender":"Female"},
+{"id":76,"name":"Andie","email":"arobben23@time.com","gender":"Female"},
+{"id":77,"name":"Prisca","email":"pbarnsdall24@biglobe.ne.jp","gender":"Female"},
+{"id":78,"name":"Dari","email":"dmacfarland25@google.ca","gender":"Female"},
+{"id":79,"name":"Ryley","email":"rrodder26@go.com","gender":"Male"},
+{"id":80,"name":"Christoph","email":"carundell27@ftc.gov","gender":"Male"},
+{"id":81,"name":"Dev","email":"dmorais28@chron.com","gender":"Male"},
+{"id":82,"name":"Berget","email":"bmattingley29@mayoclinic.com","gender":"Female"},
+{"id":83,"name":"Anallise","email":"amathias2a@rambler.ru","gender":"Female"},
+{"id":84,"name":"Helaine","email":"halcoran2b@bizjournals.com","gender":"Female"},
+{"id":85,"name":"Sherline","email":"smccaughren2c@de.vu","gender":"Female"},
+{"id":86,"name":"Tami","email":"tshorte2d@opera.com","gender":"Female"},
+{"id":87,"name":"Jabez","email":"jlarchier2e@state.gov","gender":"Male"},
+{"id":88,"name":"Terrel","email":"thaythorne2f@oracle.com","gender":"Non-binary"},
+{"id":89,"name":"Harv","email":"hperrycost2g@blogger.com","gender":"Male"},
+{"id":90,"name":"Joleen","email":"jyo2h@51.la","gender":"Female"},
+{"id":91,"name":"Bernadine","email":"bjosefer2i@elegantthemes.com","gender":"Female"},
+{"id":92,"name":"Jeromy","email":"jstrettell2j@arstechnica.com","gender":"Male"},
+{"id":93,"name":"Washington","email":"wmcneigh2k@google.co.jp","gender":"Male"},
+{"id":94,"name":"Fabio","email":"fgosart2l@1688.com","gender":"Male"},
+{"id":95,"name":"Rainer","email":"rsambiedge2m@npr.org","gender":"Male"},
+{"id":96,"name":"Bendick","email":"bgothliff2n@elegantthemes.com","gender":"Male"},
+{"id":97,"name":"Kerry","email":"kbilsborrow2o@theguardian.com","gender":"Female"},
+{"id":98,"name":"Sharleen","email":"smaclaine2p@narod.ru","gender":"Female"},
+{"id":99,"name":"Garnette","email":"gabbett2q@nydailynews.com","gender":"Female"},
+{"id":100,"name":"Peirce","email":"pswinley2r@amazon.de","gender":"Male"}]
+
+function display_data(){
+    let rows=''
+    for( emp of employees){
+        rows=rows+ `<tr>
+                        <td>${emp.id}</td>
+                        <td>${emp.name}</td>
+                        <td>${emp.email}</td>
+                        <td>${emp.gender}</td>
+                    </tr>`
+    }
+    document.getElementById("abc").innerHTML=rows;
+}
+
+    let femalecount=0;
+    for(employee of employees){
+        if (employee.gender ==="Female"){
+        femalecount++
+        }
+    document.getElementById("123").innerHTML="female count: "+femalecount ;
+} 
+
+   
+let malecount=0;
+for (employee of employees){
+    if (employee.gender ==="Male"){
+    
+     malecount++
+    }
+    document.getElementById("321").innerHTML="female count: "+malecount ;
+}
