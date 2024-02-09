ef914cb838fe36d367868083e07b63d719e8b5ac
KN-sunil <sunilsun837@gmail.com>
2024-02-09 13:18:01 +0530

    ...

---
+++ stringliteral/one.js
@@ -0,0 +1,8 @@
+//Defined as to read variable in the form of string
+//syntax()
+
+var name="sunil kn";
+var sal=50000;
+//console.log("employee:"+name + "  employee sal:"+sal);//converting this line into string literal
+
+console.log(`employee name: ${name} and salaray: ${sal}`)
